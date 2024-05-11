78839e69d25312a79c796dd183e0aed3eafa6756
beit09 <168442276+beit09@users.noreply.github.com>
2024-05-11 16:07:02 +0300

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	cin >> a;
+	cin >> b;
+	int sum = a + b;
+	int dif = a - b;
+	int pro = a * b;
+	int quo = a / b;
+	cout << sum << " " << dif << " " << pro << " " << quo;
+	return 0;
+}
